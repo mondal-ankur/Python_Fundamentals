8aed9b6d05f8cb28a8166da524ea8f46ec24020b
mondal-ankur <58561812+mondal-ankur@users.noreply.github.com>
2021-03-24 03:42:01 +0530

    Add files via upload
    Code for One Hot Encoding

---
+++ one_hot.py
@@ -0,0 +1,15 @@
+def one_hot(df,col):
+    #if index == True:
+    for i in df[col].unique():#creating a column for each unicque value of the column
+        df[i]=0 #a column with all zero
+        for j in range(df.shape[0]):
+            if df[col][j]==i:
+                df[i][j] = 1
+                #else:
+                    #df[i][j] = 0
+    df.drop(i,axis=1, inplace=True) #droping any one column
+    return df
+
+    #allow to add prefix
+    #allow multiple column
+    #reduce time complexity
