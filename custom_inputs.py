2b38dc49f133959bbf56cf216e98f42049b17dca
mondal-ankur <58561812+mondal-ankur@users.noreply.github.com>
2021-04-04 12:24:55 +0530

    Update custom_inputs.py

--- custom_inputs.py
+++ custom_inputs.py
@@ -2,6 +2,7 @@
 #na: rest will be converted to na  
 def cust_input(d, na=None):
     lst=input("Enter all the numbers with a space. Eg. 56 656 1 ....").split()
+    lst=[i.strip() for i in lst]
     for i in range(len(lst)):
         try:
             lst[i]=d(lst[i])
