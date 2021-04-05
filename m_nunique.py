29e179e00dc2eda1c89336d78aabe9b1214c10a3
Ankur Mondal <58561812+mondal-ankur@users.noreply.github.com>
2021-04-05 12:34:08 +0530

    Update m_nunique.py

--- m_nunique.py
+++ m_nunique.py
@@ -9,7 +9,7 @@ def m_nunique(df, min=0,max=10):
     count = 0
     a=dict(df.isna().sum()) # coloumn name and its null values pair
     for i  in df.columns:
-        if len(df[i].unique())>=min and len(df[i].unique())<=max: #min and max number of unique values allowed
+        if len(df[i].unique()) in range(min,max+1): #min and max number of unique values allowed
             count+=1
             if a[i]>0: #If there is null values in the coloumn
                 print(bold_start, i,"-",len(df[i].unique())-1,bold_end,"[Has",a[i]," Null Value]" , end="\n\n") #getting total number of unique values in a coloumn
